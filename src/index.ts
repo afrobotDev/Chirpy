deb10de1904090bbcafed11249aa734d9a0741c8
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-20 20:52:03 +0300

    fix: correct port number in server startup log message

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(8080, () => {
-  console.log("Server is running on http://localhost:800");
+  console.log("Server is running on http://localhost:8080");
 });
