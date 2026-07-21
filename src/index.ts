f4be4855bc51135a8afaa70bc3aa031b678768f7
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-21 06:01:59 +0300

    feat: serve logo.png via explicit route with res.sendFile instead of express.static

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
-
+import path from "path";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("assets"));
+//app.use(express.static("../assets"));
+
+app.get("/assets/logo.png", (req, res) => {
+  res.sendFile(path.resolve("../assets/logo.png"));
+});
 
 app.listen(8080, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
