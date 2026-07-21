934847834812c12f73addc569ba2b6c52caed73c
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-21 05:43:37 +0300

    fix: use directory path instead of file path for express.static

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("/assets/logo.png"));
+app.use(express.static("assets"));
 
 app.listen(8080, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
