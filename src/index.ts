86eac39acc6a5aef590b4ad9f2e570b34f84bf57
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-21 05:29:53 +0300

    feat: add static file serving for assets directory

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use(express.static("/assets/logo.png"));
 
 app.listen(8080, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
