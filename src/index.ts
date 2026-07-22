d1548de697a89b80028b1fffde3c1fbc9845f03b
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-22 09:32:11 +0300

    feat: add global error handling middleware and use next(err) for validation errors

--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,17 @@ const middlewareresetReqs = function (
   res.sendStatus(200);
 };
 
+// error handling middleware
+function handleError(
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
+  console.log(err.message);
+  return res.status(500).json({ error: "Something went wrong on our end" });
+}
+
 // api endpoints
 app.get("/api/healthz", (_req: Request, res: Response) => {
   res.set({
@@ -83,23 +94,31 @@ app.use("/", middlewareLogResponse);
 app.use("/admin/metrics", middlewareNumReqs);
 app.post("/admin/reset", middlewareresetReqs);
 
-app.post("/api/validate_chirp", (req: Request, res: Response) => {
-  if (!req.body || typeof req.body.body !== "string") {
-    return res.status(400).send({ error: "something went wrong" });
-  }
-  const MAX_CHIRP_LENGTH = 140;
-  if (req.body.body.length > MAX_CHIRP_LENGTH)
-    return res.status(400).send({ error: "Chirp is too long" });
+app.post(
+  "/api/validate_chirp",
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.body || typeof req.body.body !== "string") {
+      return res.status(400).send({ error: "something went wrong" });
+    }
+    const MAX_CHIRP_LENGTH = 140;
+    if (req.body.body.length > MAX_CHIRP_LENGTH) {
+      // res.status(400).send({ error: "Chirp is too long" })
+      const err = new Error("Chirp is too long");
+      return next(err);
+    }
 
-  const BANNED_WORDS = ["kerfuffle", "sharbert", "fornax"];
-  const cleanedBody = req.body.body
-    .toLowerCase()
-    .split(" ")
-    .map((word: string) => (BANNED_WORDS.includes(word) ? "****" : word))
-    .join(" ");
+    const BANNED_WORDS = ["kerfuffle", "sharbert", "fornax"];
+    const cleanedBody = req.body.body
+      .toLowerCase()
+      .split(" ")
+      .map((word: string) => (BANNED_WORDS.includes(word) ? "****" : word))
+      .join(" ");
 
-  return res.status(200).send({ cleanedBody: `${cleanedBody}` });
-});
+    return res.status(200).send({ cleanedBody: `${cleanedBody}` });
+  },
+);
+
+app.use(handleError);
 
 app.listen(8080, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
