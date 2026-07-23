082d33cd662591f42f7a6b3989cf6139dc51ba1c
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-23 23:58:17 +0300

    feat: add PLATFORM environment variable to APIConfig

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
 export type APIConfig = {
   fileServerHits: number;
   dbURL: string;
+  PLATFORM: string;
 };
 
 export const config: APIConfig = {
   fileServerHits: 0,
   dbURL: process.env.DB_URL ?? "",
+  PLATFORM: process.env.PLATFORM ?? "",
 };
