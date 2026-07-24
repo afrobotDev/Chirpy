f91c97c819b777d434c69b58fdb80f29b1f150a5
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-24 09:44:24 +0300

    feat: add createChirp query function

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,5 +1,5 @@
 import { db } from "../index.js";
-import { type NewUser, users } from "../schema.js";
+import { type NewUser, type NewChirp, users, chirps } from "../schema.js";
 
 export async function createUser(user: NewUser) {
   const [result] = await db
@@ -12,4 +12,9 @@ export async function createUser(user: NewUser) {
 
 export async function deleteUsers() {
   await db.delete(users);
+}
+
+export async function createChirp(chirp: NewChirp) {
+  const [result] = await db.insert(chirps).values(chirp).returning();
+  return result;
 }
