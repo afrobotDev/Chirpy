47ff68507caf22b587dfdc4b11a225be945eb666
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-24 09:44:20 +0300

    feat: add chirps table with foreign key to users and cascade delete

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,10 @@
-import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  timestamp,
+  varchar,
+  uuid,
+  foreignKey,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -10,4 +16,26 @@ export const users = pgTable("users", {
   email: varchar("email", { length: 256 }).notNull().unique(),
 });
 
+export const chirps = pgTable(
+  "chirps",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    createdAt: timestamp("createdAt").notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt")
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => new Date()),
+    body: varchar("body").notNull(),
+    userId: uuid("userId").notNull(),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.userId],
+      foreignColumns: [users.id],
+      name: "custom_fk",
+    }).onDelete("cascade"),
+  ],
+);
+
 export type NewUser = typeof users.$inferInsert;
+export type NewChirp = typeof chirps.$inferInsert;
