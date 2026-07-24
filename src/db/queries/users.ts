355ef1bd608ecc6b51a5c9bcc8a6d5c131a12e63
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-24 07:59:29 +0300

    refactor: remove unused parameter from deleteUsers and simplify query

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -10,6 +10,6 @@ export async function createUser(user: NewUser) {
   return result;
 }
 
-export async function deleteUsers(user: NewUser) {
-  await db.delete(users).where();
+export async function deleteUsers() {
+  await db.delete(users);
 }
